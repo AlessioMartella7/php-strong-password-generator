99b44a08c33fc9d6de8338bf0c303e455d67d84e
AlessioMartella7 <alessio.mart87@gmail.com>
2024-10-08 14:56:34 +0200

    refactored functions.php

--- functions/functions.php
+++ functions/functions.php
@@ -1,7 +1,7 @@
 <?php
 
 function generateRandomString ($length) {
-    if(!empty($length) && ($length > 1) && is_numeric($length) &&($length <= 16)){
+    if(isset($length) && !empty($length) && ($length > 1) && is_numeric($length) &&($length <= 16)){
     return base64_encode(random_bytes($length));
     } else {
         return false;
